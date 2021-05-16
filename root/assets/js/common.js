da7ede1a328ec3190fead5cf59092c43059ce27a
ducvu@1991 <vund@piotech.biz>
2021-05-16 10:43:54 +0700

    tunngt

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -45,4 +45,4 @@ document.addEventListener("DOMContentLoaded", function(){
   
   }); 
 // DOMContentLoaded  end
-var swiper = new Swiper('.swiper-container');
+
---
+++ root/assets/js/swiper.js
@@ -0,0 +1,12 @@
+
+var swiper = new Swiper(".swiper-container", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    effect: "fade",
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+  });
