b472311ba1192ab2a96832a594033ca5aaac5f00
ducvu@1991 <vund@piotech.biz>
2021-05-19 00:40:45 +0700

    tungnt

--- root/assets/js/swiper.js
+++ root/assets/js/swiper.js
@@ -1,8 +1,9 @@
 
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".swiper-banner", {
     spaceBetween: 30,
     centeredSlides: true,
     loop: true,
+    lazy: true,
     loopFillGroupWithBlank: true,
     effect: "fade",
     autoplay: {
@@ -10,3 +11,23 @@ var swiper = new Swiper(".swiper-container", {
       disableOnInteraction: false,
     },
   });
+  var swiper = new Swiper('.customer-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    lazy: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    }
+  });  
