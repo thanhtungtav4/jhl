da634b5893ef696fb1feec44b7606518579565f6
tungnt <nguyenthanhtung74119@gmail.com>
2021-10-21 13:07:39 +0700

    menu add event hover

--- root/assets/js/common.js
+++ root/assets/js/common.js
@@ -44,29 +44,10 @@ document.addEventListener("DOMContentLoaded", function(){
   });
   
   }); 
-// DOMContentLoaded  end
-$(function(){
-  
-  $('li.dropdown > a').on('click',function(event){
-    
-    event.preventDefault();
-    $(this).toggleClass('selected');
-    $(this).parent().find('ul').first().toggle(300);
-    
-    $(this).parent().siblings().find('ul').hide(200);
-    
-    //Hide menu when clicked outside
-    $(this).parent().find('ul').parent().mouseleave(function(){ 
-      var thisUI = $(this);
-      $('html').click(function(){
-        thisUI.children(".dropdown-menu").hide();
-    thisUI.children("a").removeClass('selected');
-               
-        $('html').unbind('click');
-      });
-    });
-    
-    
+$(function(){ 
+  $('.m-header__menu li').hover(function(){
+    $(this).find('ul').first().stop().toggle(200);
+  }, function(){
+    $(this).find('ul').stop().hide(200);
   });
-  
 });
